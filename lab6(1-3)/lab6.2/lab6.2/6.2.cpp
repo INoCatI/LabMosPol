8002bf021cdc492370125b69216b120d0f5e064a
INoCatI <90762272+INoCatI@users.noreply.github.com>
2021-10-01 00:10:18 +0300

    Add files via upload

---
+++ lab6(1-3)/lab6.2/lab6.2/6.2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+char A = 'A', B = 'B',C='C', n; //A--B	B--C	C--A
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	n = A;
+	A=B;
+	B = C;
+	C = n;
+	cout << "В переменной А хранится: " << A << "\n" << "В переменной B хранится: " << B << "\n" << "В переменной C хранится: " << C << endl;
+}
